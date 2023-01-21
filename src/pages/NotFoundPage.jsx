413b7784ff23faa8c4a11a99ee3cce7e74c282cb
pakapol12704@gmail.com <96942223+PTMeen@users.noreply.github.com>
2023-01-21 15:59:57 +0700

    build not ound page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,16 @@
+import { Center, Heading, Link, VStack } from "@chakra-ui/react";
+import { Link as RLink } from "react-router-dom";
+
 const NotFOundPage = () => {
-  return <div>NotFOundPage</div>;
+  return (
+    <Center minH="80vh">
+      <VStack>
+        <Heading as="h1">Page not found</Heading>
+        <Link as={RLink} to="/" _hover={{ color: "cyan.500" }}>
+          Go Home
+        </Link>
+      </VStack>
+    </Center>
+  );
 };
 export default NotFOundPage;
